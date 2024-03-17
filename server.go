4c207375fce43322720b1a175a30b94ca4f4a597
MrKuznetsov698 <mr.kuznetsov698@gmail.com>
2024-03-17 13:23:35 +0300

    Added favicon, downloaded "bootstrap.min.css", added some comments

--- server.go
+++ server.go
@@ -9,15 +9,20 @@ import (
 )
 
 func main() {
+	// parse config
 	config := ParseConfig("config.yaml")
 	slices.SortFunc(config.UnsolvedBorders, func(a, b *UnsolvedBorder) int {
 		return a.Count - b.Count
 	})
 
+	// router
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/page.html")
+	// data
 	criterionTitles, usersValues := GetDeadlineResults(config)
 	lastUpdate := time.Now()
+	// routes
+	router.Static("/static", "./static")
 	router.GET("/", func(c *gin.Context) {
 		if time.Since(lastUpdate).Seconds() > config.CacheTime {
 			criterionTitles, usersValues = GetDeadlineResults(config)
