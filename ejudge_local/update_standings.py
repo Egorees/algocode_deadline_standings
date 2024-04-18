2f53859e5c7015ed9d6cec1b5156969735f35bdc
mrKuznetsov698 <mrKuznetsov698@gmail.com>
2024-04-18 16:52:08 +0300

    Fixed borders & delete ejudge_local

--- ejudge_local/update_standings.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-import requests
-import json
-
-standings = requests.get('https://algocode.ru/standings_data/bp_fall_2023/')
-with open('human_standings.json', 'w', encoding='utf-8') as fl:
-    json.dump(standings.json(), fl, indent=2, ensure_ascii=False)
-with open('raw_standings.json', 'wb') as fl:
-    fl.write(standings.content)
